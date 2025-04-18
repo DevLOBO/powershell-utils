18d6e4b50a96236fcfe7f4b27cdcc47928044932
Franco Hernandez <franco.hernandezh@my.unitec.edu.mx>
2025-04-18 09:14:09 -0600

    New function for mvn tests

--- Modules/SpringBoot/SpringBoot.psm1
+++ Modules/SpringBoot/SpringBoot.psm1
@@ -37,7 +37,54 @@ function Use-SpringDevDebug {
 	mvn spring-boot:run -Ddebug
 }
 
+function Analyze-SurefireReports {
+    mvn test > $null
+
+    # Define la ruta a la carpeta de reportes de Surefire
+    $reportPath = "target/surefire-reports"
+
+    # Busca todos los archivos XML en la carpeta de reportes
+    $reportFiles = Get-ChildItem -Path $reportPath -Filter "*.xml"
+
+    if ($reportFiles.Count -eq 0) {
+        Write-Warning "No se encontraron archivos de reporte Surefire en '$reportPath'."
+        return
+    }
+
+    $failedTests = 0
+    foreach ($reportFile in $reportFiles) {
+        Write-Host "`n--- Reporte: $($reportFile.Name) ---"
+
+        # Lee el contenido del archivo XML
+        [xml]$xmlReport = Get-Content -Path $reportFile.FullName
+
+        # Itera sobre cada etiqueta <testcase>
+        foreach ($testcase in $xmlReport.testsuite.testcase) {
+            $testName = $testcase.name
+            $executionTime = $testcase.time
+
+            if ($testcase.failure) {
+                $failedTests += 1
+                Write-Host "[FAILED] $($testName) - $($executionTime)s"
+                Write-Host "   Mensaje de Fallo: $($testcase.failure.message)"
+                if ($testcase.failure.type) {
+                    Write-Host "   Tipo de Fallo: $($testcase.failure.type)"
+                }
+            } else {
+                Write-Host "[SUCCESS] $($testName) - $($executionTime)s"
+            }
+        }
+    }
+
+    if ($failedTests -gt 0) {
+        Select-Beep Fail
+    } else {
+        Select-Beep Success
+    }
+}
+
 New-Alias -Name srun -Value Use-SpringDev
 New-Alias -Name srundebug -Value Use-SpringDevDebug
 New-Alias -Name bimgnat -Value New-NativeImage
 New-Alias -Name spmm -Value Monitor-SpringBootActuatorMetrics
+New-Alias -Name spt -Value Analyze-SurefireReports
